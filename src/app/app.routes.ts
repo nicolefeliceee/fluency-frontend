3294b85cbcb7060265fc57d3eda4d15a9628c812
nicolefeliceee <nicolefeliceee@gmail.com>
2025-01-04 19:46:25 +0700

    header done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {path:'', component: LandingPageComponent},
+  {path:'home', component: HomeComponent},
   {path:'signup', component: SignupComponent},
   {path:'header', component: HeaderComponent}
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,RouterOutlet,RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isDropdownVisible: boolean = false;
+
+  toggleDropdown(event: MouseEvent): void {
+    // Toggle dropdownnya
+    event.stopPropagation();
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(): void {
+    // Close the dropdown if clicking outside
+    this.isDropdownVisible = false;
+  }
+
 
 }
