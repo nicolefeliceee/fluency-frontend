4085a021edf1aaccb37d6a94c9d6aa3649b74553
devindevin278 <devindevin278@gmail.com>
2025-01-04 23:06:48 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CompleteProfileComponent } from './pages/signup/signup-forms/complete-profile/complete-profile.component';
+import { ChooseCategoryComponent } from './pages/signup/signup-forms/choose-category/choose-category.component';
+import { BrandTargetMarketComponent } from './pages/signup/signup-forms/brand-target-market/brand-target-market.component';
 
 export const routes: Routes = [
   {path:'', component: LandingPageComponent},
-  {path:'signup', component: SignupComponent}
+  {path:'home', component: HomeComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'login', component: LoginComponent},
+  {path:'about-us', component: AboutUsComponent},
+  {path:'header', component: HeaderComponent},
+  { path: 'signup/brand/profile', component: CompleteProfileComponent },
+  { path: 'signup/brand/category', component: ChooseCategoryComponent },
+  { path: 'signup/brand/target', component: BrandTargetMarketComponent }
 ];
