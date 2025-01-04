fe00b32992e2139891e640ac8109c0e1503c1d5b
devindevin278 <devindevin278@gmail.com>
2025-01-04 13:39:51 +0700

    create folder and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
-  {path:'', component: LandingPageComponent}
+  {path:'', component: LandingPageComponent},
+  {path:'signup', component: SignupComponent}
 ];
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
