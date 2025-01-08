c057a33cfb1f60442b3d4ee4c52d3020620dee9b
nicolefeliceee <nicolefeliceee@gmail.com>
2025-01-08 13:09:28 +0700

    filter influencer

---
+++ src/app/components/range-filter/range-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-range-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './range-filter.component.html',
+  styleUrl: './range-filter.component.css'
+})
+export class RangeFilterComponent {
+
+}
--- src/app/pages/influencer/influencer.component.ts
+++ src/app/pages/influencer/influencer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { InfluencerCardComponent } from "../../components/influencer-card/influencer-card.component";
 import { TabComponent } from "../../components/tab/tab.component";
+import { RangeFilterComponent } from "../../components/range-filter/range-filter.component";
 
 @Component({
   selector: 'app-influencer',
   standalone: true,
-  imports: [HeaderComponent, InfluencerCardComponent, TabComponent],
+  imports: [HeaderComponent, InfluencerCardComponent, TabComponent, RangeFilterComponent],
   templateUrl: './influencer.component.html',
   styleUrl: './influencer.component.css'
 })
