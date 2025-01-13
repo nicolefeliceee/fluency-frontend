e52e073eb635f711135742951af31a02d7d69404
nicolefeliceee <nicolefeliceee@gmail.com>
2025-01-13 22:09:46 +0700

    tambahan

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8081',
+  baseUrl: 'http://localhost:8080',
   appName: 'My Angular App',
   instagramBaseUrl: 'https://graph.facebook.com/v21.0'
 };
